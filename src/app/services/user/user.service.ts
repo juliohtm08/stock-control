ccecd0f81e6076c05116845bbdf0b31dbf928fff
juliohtm08 <jhtomoyuki345@gmail.com>
2025-06-16 14:45:13 -0300

    implementando guarda de rotas para rotas protegidas

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 import { Observable } from 'rxjs';
 import { AuthRequest } from 'src/app/models/interfaces/auth/AuthRequest';
 import { AuthResponse } from 'src/app/models/interfaces/auth/AuthResponse';
@@ -13,7 +14,7 @@ import { environment } from 'src/environments/environment';
 export class UserService {
   private API_URL = environment.API_URL;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private cookie: CookieService) {}
 
   // Cadastrar novo usuário
   signupUser(requestDatas: SignupUserRequest): Observable<SignupUserResponse> {
@@ -26,5 +27,11 @@ export class UserService {
   // Autenticar usuário
   authUser(requestDatas: AuthRequest): Observable<AuthResponse> {
     return this.http.post<AuthResponse>(`${this.API_URL}/auth`, requestDatas);
+  }
+
+  // Verificar se o usuário possui um token ou cookie
+  isLoggedIn(): boolean {
+    const JWT_TOKEN = this.cookie.get('USER_INFO');
+    return JWT_TOKEN ? true : false;
   }
 }
