95213f96c6edfa1d274083614f31a0509e813874
juliohtm08 <jhtomoyuki345@gmail.com>
2025-06-16 13:49:52 -0300

    Criando módulo, componente e rota da funcionalidade do dashbaord

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/home.component';
+import { DashboardHomeComponent } from './modules/dashboard/dashboard-home/dashboard-home.component';
 
 const routes: Routes = [
   {
     path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'dashboard',
+    component: DashboardHomeComponent,
   },
 ];
 
