dc2651f97292c09bba65e24d9b861f3045bf5868
juliohtm08 <jhtomoyuki345@gmail.com>
2025-06-16 17:40:16 -0300

    criando um shared module com componente toolbar-navigation

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -14,6 +14,7 @@ import { DashboardHomeComponent } from './dashboard-home/dashboard-home.componen
 import { DASHBOARD_ROUTES } from './dashboard.routing';
 import { MessageService } from 'primeng/api';
 import { CookieService } from 'ngx-cookie-service';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [DashboardHomeComponent],
@@ -29,6 +30,8 @@ import { CookieService } from 'ngx-cookie-service';
     CardModule,
     ToastModule,
     ChartModule,
+    // shared
+    SharedModule,
   ],
   providers: [MessageService, CookieService],
 })
