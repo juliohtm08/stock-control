30d6252acb1e8c5afd3c4f81bf414f3b922e9c93
juliohtm08 <jhtomoyuki345@gmail.com>
2025-06-17 15:09:21 -0300

    Implementando funcionalidade de deletar produto

--- src/app/modules/products/components/products-table/products-table.component.ts
+++ src/app/modules/products/components/products-table/products-table.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ProductEvent } from 'src/app/models/enums/products/ProductEvent';
+import { DeleteProductAction } from 'src/app/models/interfaces/products/event/DeleteProductAction';
 import { EventAction } from 'src/app/models/interfaces/products/event/EventAction';
 import { GetAllProductsResponse } from 'src/app/models/interfaces/products/response/GetAllProductsResponse';
 
@@ -11,6 +12,7 @@ import { GetAllProductsResponse } from 'src/app/models/interfaces/products/respo
 export class ProductsTableComponent {
   @Input() products: Array<GetAllProductsResponse> = []; // recebe os dados da lista de produtos do componente pai
   @Output() productEvent = new EventEmitter<EventAction>(); // cria um canal de comunicação com o componente pai
+  @Output() deleteProductEvent = new EventEmitter<DeleteProductAction>();
 
   public productSelected!: GetAllProductsResponse;
   public addProductEvent = ProductEvent.ADD_PRODUCT_EVENT;
@@ -21,6 +23,16 @@ export class ProductsTableComponent {
     if (action && action !== '') {
       const productEventData = id && id !== '' ? { action, id } : { action };
       this.productEvent.emit(productEventData); // envia os dados para o componente pai
+    }
+  }
+
+  // evento de deletar produto
+  handleDeleteProduct(product_id: string, productName: string): void {
+    if (product_id !== '' && productName !== '') {
+      this.deleteProductEvent.emit({
+        product_id,
+        productName,
+      });
     }
   }
 }
