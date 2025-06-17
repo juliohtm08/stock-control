c323ea200ca7122659da35c008370abeddab5f6a
juliohtm08 <jhtomoyuki345@gmail.com>
2025-06-17 02:08:27 -0300

    Implementação de busca de dados de produtos

--- src/app/modules/products/page/products-home/products-home.component.ts
+++ src/app/modules/products/page/products-home/products-home.component.ts
@@ -1,8 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { Subject, takeUntil } from 'rxjs';
+import { GetAllProductsResponse } from 'src/app/models/interfaces/products/response/GetAllProductsResponse';
+import { ProductsService } from 'src/app/services/products/products.service';
+import { ProductsDataTransferService } from 'src/app/shared/services/products/products-data-transfer.service';
 
 @Component({
   selector: 'app-products-home',
   templateUrl: './products-home.component.html',
   styleUrls: [],
 })
-export class ProductsHomeComponent {}
+export class ProductsHomeComponent implements OnInit, OnDestroy {
+  private readonly destroy$: Subject<void> = new Subject();
+  public productsDatas: Array<GetAllProductsResponse> = [];
+
+  constructor(
+    private productsService: ProductsService,
+    private productsDtService: ProductsDataTransferService,
+    private router: Router,
+    private messageService: MessageService
+  ) {}
+
+  ngOnInit(): void {
+    this.getServiceProductsDatas();
+  }
+
+  // busca os dados que estão armazenados em memória,
+  // caso contrário busca direto na API
+  getServiceProductsDatas() {
+    const productsLoaded = this.productsDtService.getProductsDatas();
+
+    if (productsLoaded.length > 0) {
+      this.productsDatas = productsLoaded;
+    } else this.getAPIProductsDatas();
+
+    //console.log('DADOS DE PRODUTOS', this.productsDatas);
+  }
+
+  // busca os dados dos produtos na API
+  getAPIProductsDatas() {
+    this.productsService
+      .getAllProducts()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          if (response.length > 0) {
+            this.productsDatas = response;
+            //console.log('DADOS DE PRODUTOS', this.productsDatas);
+          }
+        },
+        error: (err) => {
+          console.log(err);
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Erro',
+            detail: 'Erro ao buscar produtos',
+            life: 2500,
+          });
+          this.router.navigate(['/dashboard']);
+        },
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
