dc1b71f88b1e4b7cf4404c9e918f2c1f3d89e572
juliohtm08 <jhtomoyuki345@gmail.com>
2025-06-25 20:48:01 -0300

    Implementando funcionalidade de editar produto

--- src/app/modules/products/components/product-form/product-form.component.ts
+++ src/app/modules/products/components/product-form/product-form.component.ts
@@ -2,11 +2,15 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
+import { DynamicDialogConfig } from 'primeng/dynamicdialog';
 import { Subject, takeUntil } from 'rxjs';
 import { GetCategoriesResponse } from 'src/app/models/interfaces/categories/response/GetCategoriesResponse';
+import { EventAction } from 'src/app/models/interfaces/products/event/EventAction';
 import { CreateProductRequest } from 'src/app/models/interfaces/products/request/CreateProductRequest';
+import { GetAllProductsResponse } from 'src/app/models/interfaces/products/response/GetAllProductsResponse';
 import { CategoriesService } from 'src/app/services/categories/categories.service';
 import { ProductsService } from 'src/app/services/products/products.service';
+import { ProductsDataTransferService } from 'src/app/shared/services/products/products-data-transfer.service';
 
 @Component({
   selector: 'app-product-form',
@@ -18,6 +22,14 @@ export class ProductFormComponent implements OnInit, OnDestroy {
   public categoriesDatas: Array<GetCategoriesResponse> = [];
   public selectedCategory: Array<{ name: string; code: string }> = [];
 
+  // ação (adicionar/editar) e dados recebidos via DynamicDialog
+  public productAction!: {
+    event: EventAction;
+    productDatas: Array<GetAllProductsResponse>;
+  };
+  public productSelectedDatas!: GetAllProductsResponse; // dados do produto selecionado para edição
+  public productsDatas!: Array<GetAllProductsResponse>; // todos os produtos carregados
+
   // Formulário com validações
   public addProductForm = this.formBuilder.group({
     name: ['', Validators.required],
@@ -26,16 +38,25 @@ export class ProductFormComponent implements OnInit, OnDestroy {
     category_id: ['', Validators.required],
     amount: [0, Validators.required],
   });
+  public editProductForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    price: ['', Validators.required],
+    description: ['', Validators.required],
+    amount: [0, Validators.required],
+  });
 
   constructor(
     private categoriesService: CategoriesService,
     private productService: ProductsService,
+    private productDtService: ProductsDataTransferService,
     private formBuilder: FormBuilder,
     private messageService: MessageService,
-    private router: Router
+    private router: Router,
+    public ref: DynamicDialogConfig
   ) {}
 
   ngOnInit(): void {
+    this.productAction = this.ref.data; // recupera dados passados pelo componente pai
     this.getAllCategories();
   }
 
@@ -53,7 +74,7 @@ export class ProductFormComponent implements OnInit, OnDestroy {
       });
   }
 
-  // método para adicionar um novo produto
+  // Submete o formulário para criar um novo produto
   handleSubmitAddProduct(): void {
     if (this.addProductForm?.value && this.addProductForm?.valid) {
       const requestCreateProduct: CreateProductRequest = {
@@ -91,6 +112,50 @@ export class ProductFormComponent implements OnInit, OnDestroy {
     }
 
     this.addProductForm.reset();
+  }
+
+  // Submete o formulário de edição de produto
+  handleSubmitEditProduct(): void {
+    /*    if (this.editProductForm.value && this.editProductForm.valid) {
+    } */
+  }
+
+  // Carrega os dados do produto selecionado com base no ID
+  getProductSelectedDatas(productId: string): void {
+    const allProducts = this.productAction?.productDatas;
+
+    if (allProducts.length > 0) {
+      const productFiltered = allProducts.filter(
+        (elemnt) => elemnt?.id === productId
+      );
+
+      if (productFiltered) {
+        this.productSelectedDatas = productFiltered[0]; // armazena o produto encontrado
+
+        this.editProductForm.setValue({
+          name: this.productSelectedDatas?.name,
+          price: this.productSelectedDatas?.price,
+          amount: this.productSelectedDatas?.amount,
+          description: this.productSelectedDatas?.description,
+        });
+      }
+    }
+  }
+
+  // Carrega todos os produtos e atualiza o serviço de compartilhamento
+  getProductDatas(): void {
+    this.productService
+      .getAllProducts()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          if (response.length) {
+            this.productsDatas = response; // armazena os produtos
+            this.productsDatas &&
+              this.productDtService.setProductsDatas(this.productsDatas); // atualiza os dados compartilhados
+          }
+        },
+      });
   }
 
   ngOnDestroy(): void {
