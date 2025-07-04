61fae26a8fceae58ff35cdceae1124e209573e7b
juliohtm08 <jhtomoyuki345@gmail.com>
2025-07-04 15:21:26 -0300

    Implementando funcionalidade de deletar categoria

--- src/app/modules/categories/page/categories-home/categories-home.component.ts
+++ src/app/modules/categories/page/categories-home/categories-home.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { ConfirmationService, MessageService } from 'primeng/api';
 import { DialogService } from 'primeng/dynamicdialog';
 import { Subject, takeUntil } from 'rxjs';
+import { DeleteCategoryAction } from 'src/app/models/interfaces/categories/event/DeleteCategory';
 import { GetCategoriesResponse } from 'src/app/models/interfaces/categories/response/GetCategoriesResponse';
 import { CategoriesService } from 'src/app/services/categories/categories.service';
 
@@ -48,6 +49,47 @@ export class CategoriesHomeComponent implements OnInit, OnDestroy {
           this.router.navigate(['/dashboard']);
         },
       });
+  }
+
+  handleDeleteCategoryAction(event: DeleteCategoryAction): void {
+    if (event) {
+      this.confirmationService.confirm({
+        message: `Confirma a exclusão da categoria: ${event?.categoryName}?`,
+        header: 'Confirmação de exclusão',
+        icon: 'pi pi-exclamation-triangle',
+        acceptLabel: 'Sim',
+        rejectLabel: 'Não',
+        accept: () => this.deleteCategory(event?.category_id),
+      });
+    }
+  }
+
+  deleteCategory(category_id: string): void {
+    if (category_id) {
+      this.categoriesService
+        .deleteCategory({ category_id })
+        .pipe(takeUntil(this.destroy$))
+        .subscribe({
+          next: (response) => {
+            this.messageService.add({
+              severity: 'success',
+              summary: 'Sucesso',
+              detail: 'Categoria removida com sucesso',
+              life: 2500,
+            });
+          },
+          error: (err) => {
+            console.log(err);
+            this.messageService.add({
+              severity: 'error',
+              summary: 'Erro',
+              detail: 'Erro ao remover categoria',
+              life: 2500,
+            });
+          },
+        });
+      this.getAllCategories();
+    }
   }
 
   ngOnDestroy(): void {
