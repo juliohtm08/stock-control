80356a16968cd966e60bc03eec3778bb401f8326
juliohtm08 <jhtomoyuki345@gmail.com>
2025-07-05 15:02:15 -0300

    implementando funcionalidade de adicionar categoria - parte 2

--- src/app/modules/categories/page/categories-home/categories-home.component.ts
+++ src/app/modules/categories/page/categories-home/categories-home.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ConfirmationService, MessageService } from 'primeng/api';
-import { DialogService } from 'primeng/dynamicdialog';
+import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { Subject, takeUntil } from 'rxjs';
 import { DeleteCategoryAction } from 'src/app/models/interfaces/categories/event/DeleteCategory';
 import { GetCategoriesResponse } from 'src/app/models/interfaces/categories/response/GetCategoriesResponse';
+import { EventAction } from 'src/app/models/interfaces/products/event/EventAction';
 import { CategoriesService } from 'src/app/services/categories/categories.service';
+import { CategoriesFormComponent } from '../../components/categories-form/categories-form.component';
 
 @Component({
   selector: 'app-categories-home',
@@ -14,6 +16,7 @@ import { CategoriesService } from 'src/app/services/categories/categories.servic
 })
 export class CategoriesHomeComponent implements OnInit, OnDestroy {
   private readonly destroy$: Subject<void> = new Subject();
+  private ref!: DynamicDialogRef;
   public categoriesDatas: Array<GetCategoriesResponse> = [];
 
   constructor(
@@ -73,6 +76,7 @@ export class CategoriesHomeComponent implements OnInit, OnDestroy {
         .pipe(takeUntil(this.destroy$))
         .subscribe({
           next: (response) => {
+            this.getAllCategories();
             this.messageService.add({
               severity: 'success',
               summary: 'Sucesso',
@@ -81,6 +85,7 @@ export class CategoriesHomeComponent implements OnInit, OnDestroy {
             });
           },
           error: (err) => {
+            this.getAllCategories();
             console.log(err);
             this.messageService.add({
               severity: 'error',
@@ -91,6 +96,25 @@ export class CategoriesHomeComponent implements OnInit, OnDestroy {
           },
         });
       this.getAllCategories();
+    }
+  }
+
+  handleCategoryAction(event: EventAction): void {
+    if (event) {
+      this.ref = this.dialogService.open(CategoriesFormComponent, {
+        header: event?.action,
+        width: '70%',
+        contentStyle: { overflow: 'auto' },
+        baseZIndex: 10000,
+        maximizable: true,
+        data: {
+          event: event,
+        },
+      });
+
+      this.ref.onClose.pipe(takeUntil(this.destroy$)).subscribe({
+        next: () => this.getAllCategories(),
+      });
     }
   }
 
